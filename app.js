84f5cd19b666157090406ef10a14aed24066402a
Muchaveleli Manjate <1653288@students.wits.ac.za>
2022-05-22 01:22:39 +0200

    Create account, button and form added

--- app.js
+++ app.js
@@ -16,5 +16,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/createAcc', indexRouter);
+app.use('/newAcc', indexRouter);
 
 module.exports = app;
