0952a427de38ed1cd17bdd8551bb1fdecffb1803
Christina Mathipa <1856076@students.wits.ac.za>
2022-05-22 01:31:59 +0200

    Added SpaceWordle play grid and keyboard

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var playRouter = require('./routes/play');
 
 var app = express();
 
@@ -16,5 +17,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/play', playRouter);
 
 module.exports = app;
+app.listen(3000)
+console.log('Express server running on port 3000')
+
