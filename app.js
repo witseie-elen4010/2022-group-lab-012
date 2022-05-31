646b989e11ea9dbe9e542ed46294ea8f4d42da53
Thabo-PROG <thabott73@gmail.com>
2022-05-31 22:41:10 +0200

    Solve Conflicts on App.js and account.html

--- app.js
+++ app.js
@@ -71,15 +71,5 @@ app.get('/check', (req, res) => {
 })
 
 module.exports = app;
-app.listen(process.env.PORT || 8000)
-console.log('Express server running on port 3000',process.env.PORT || 8000)
-=======
-
-app.use('/createAcc', indexRouter);
-app.use('/newAcc', indexRouter);
-app.use('/play', playRouter);
-
-
-module.exports = app;
 app.listen(3000)
 console.log('Express server running on port 3000')
